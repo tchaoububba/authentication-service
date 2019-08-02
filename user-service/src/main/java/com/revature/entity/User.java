a7e8096afbc7c0b962c20597e0aa0ead23ae6c2d
Saul <sauleff@gmail.com>
2019-08-02 11:47:40 -0400

    Adding DB properties

--- user-service/src/main/java/com/revature/entity/User.java
+++ user-service/src/main/java/com/revature/entity/User.java
@@ -42,8 +42,158 @@ public class User {
 	
 	
 public User() { }
-	
-	
+
+
+
+
+public User(String email, String password) {
+	super();
+	this.email = email;
+	this.password = password;
+}
+
+
+public User(String resetToken) {
+	super();
+	this.resetToken = resetToken;
+}
+
+
+public User(long id, String email, String password, String firstName, String lastName, Date createdDate,
+		String resetToken, String role) {
+	super();
+	this.id = id;
+	this.email = email;
+	this.password = password;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.createdDate = createdDate;
+	this.resetToken = resetToken;
+	this.role = role;
+}
+
+
+
+
+public long getId() {
+	return id;
+}
+
+
+
+
+public void setId(long id) {
+	this.id = id;
+}
+
+
+
+
+public String getEmail() {
+	return email;
+}
+
+
+
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+
+
+public String getPassword() {
+	return password;
+}
+
+
+
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+
+
+
+public String getFirstName() {
+	return firstName;
+}
+
+
+
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+
+
+
+public String getLastName() {
+	return lastName;
+}
+
+
+
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+
+
+
+public Date getCreatedDate() {
+	return createdDate;
+}
+
+
+
+
+public void setCreatedDate(Date createdDate) {
+	this.createdDate = createdDate;
+}
+
+
+
+
+public String getResetToken() {
+	return resetToken;
+}
+
+
+
+
+public void setResetToken(String resetToken) {
+	this.resetToken = resetToken;
+}
+
+
+
+
+public String getRole() {
+	return role;
+}
+
+
+
+
+public void setRole(String role) {
+	this.role = role;
+}
+
+
+
+
+@Override
+public String toString() {
+	return "User [id=" + id + ", email=" + email + ", password=" + password + ", firstName=" + firstName + ", lastName="
+			+ lastName + ", createdDate=" + createdDate + ", resetToken=" + resetToken + ", role=" + role + "]";
+}
+
+
 	
 	
 }
