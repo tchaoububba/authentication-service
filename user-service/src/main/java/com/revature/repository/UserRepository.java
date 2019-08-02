3318d8dafb888e582ac147f183dacd61761d1413
Ryan <Ryanfradj09@gmail.com>
2019-08-02 13:35:07 -0400

    dto

--- user-service/src/main/java/com/revature/repository/UserRepository.java
+++ user-service/src/main/java/com/revature/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.revature.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.revature.entity.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 	
+	 /**
+	  * 
+	  * @param email
+	  * @return A full user object is returned in connection with the email. If no user is found with the parameter email
+	  * 		associated to it the method will return null
+	  */
+	 @Query("select u from User u where u.email = :email")
+	    User findByEmailReturnStream(String email);
 
 }
