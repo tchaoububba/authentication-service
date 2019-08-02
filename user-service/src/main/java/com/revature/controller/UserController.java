25e57c187884cfafcaf1d18d93e23d19262301e5
Ryan <Ryanfradj09@gmail.com>
2019-08-02 14:55:53 -0400

    login done

--- user-service/src/main/java/com/revature/controller/UserController.java
+++ user-service/src/main/java/com/revature/controller/UserController.java
@@ -1,20 +1,29 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.entity.User;
 import com.revature.service.UserService;
 
 @RestController
 @RequestMapping("/api")
-public class UserRestController {
+public class UserController {
 	
 	UserService userService;
 	
 	@Autowired
-	public UserRestController(UserService userService) {
+	public UserController(UserService userService) {
 		this.userService=userService;
+	}
+	
+	@PostMapping(value="/login")
+	public User login(@RequestBody User user) {
+	
+		return userService.Authenticate(user);
 	}
 
 }
