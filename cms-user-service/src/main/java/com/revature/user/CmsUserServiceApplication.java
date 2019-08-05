31b5c401f401ad2b72417f15d452c607b0968d52
Saul <sauleff@gmail.com>
2019-08-05 13:36:52 -0400

    Creating new project

---
+++ cms-user-service/src/main/java/com/revature/user/CmsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsUserServiceApplication.class, args);
+	}
+
+}
